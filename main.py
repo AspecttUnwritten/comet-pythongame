b0764bf7ead34b20a83b1d5a6b632b0ed7b0ca10
AspectUnwritten <johnnybravobitch@tutamail.com>
2026-01-17 12:41:42 +0100

    finally done with this fucking game

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import sys
+from logging import log
+from turtle import up
+
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_event, log_state
 from player import Player
+from shot import Shot
 
 
 def main():
@@ -15,24 +22,45 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    x = SCREEN_WIDTH/2
-    y = SCREEN_HEIGHT/2
-    player = Player(x,y)
+    # new groups
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, drawable, updatable)
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    field = AsteroidField()
 
     while True:
         log_state()
 
-        player.update(dt)
-
+        updatable.update(dt)
+        for a in asteroids:
+            if player.collides_with(a) is True:
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
+        for sprite in drawable:
+            sprite.draw(screen)
 
+        for a in asteroids:
+            for b in shots:
+                if a.collides_with(b):
+                    log_event("asteroid_shot")
+                    a.split()
+                    b.kill()
+        pygame.display.flip()
 
         dt = clock.tick(60) / 1000
         # print(dt)
--- player.py
+++ player.py
@@ -1,11 +1,23 @@
-from circleshape import CircleShape
-from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 
+from circleshape import CircleShape
+from constants import (
+    LINE_WIDTH,
+    PLAYER_RADIUS,
+    PLAYER_SHOOT_COOLDOWN_SECONDS,
+    PLAYER_SHOT_SPEED,
+    PLAYER_SPEED,
+    PLAYER_TURN_SPEED,
+    SHOT_RADIUS,
+)
+from shot import Shot
+
+
 class Player(CircleShape):
-    def __init__(self,x,y):
-        super().__init__(x,y,PLAYER_RADIUS)
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     # in the Player class
     def triangle(self):
@@ -17,26 +29,35 @@ class Player(CircleShape):
         return [a, b, c]
 
     def draw(self, screen):
-        pygame.draw.polygon(screen,"white", self.triangle(), LINE_WIDTH)
+        pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
 
     def rotate(self, dt):
-        self.rotation += (PLAYER_TURN_SPEED * dt)
-
+        self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer -= dt
 
         if keys[pygame.K_a]:
-             self.rotate(dt)
+            self.rotate(dt)
         if keys[pygame.K_d]:
-             self.rotate(-dt)
+            self.rotate(-dt)
         if keys[pygame.K_w]:
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            if self.timer <= 0:
+                self.shoot()
+                self.timer = PLAYER_SHOOT_COOLDOWN_SECONDS
 
-    def move(self,dt):
+    def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        direction = pygame.Vector2(0, 1).rotate(self.rotation)
+        shot.velocity = direction * PLAYER_SHOT_SPEED
